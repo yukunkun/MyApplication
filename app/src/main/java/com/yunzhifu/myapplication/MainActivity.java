01f05a3a57db26bb6fcf51f0763abd59af120eb1
huwenming <wenming.hu@urovo.com>
2020-05-27 16:10:32 +0800

    update

--- app/src/main/java/com/yunzhifu/myapplication/MainActivity.java
+++ app/src/main/java/com/yunzhifu/myapplication/MainActivity.java
@@ -3,13 +3,16 @@ package com.yunzhifu.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mTextView = findViewById(R.id.tv);
     }
 }

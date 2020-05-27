81121ee2c5b1ee0e221092cd26ef228eb125e566
yukunkun <1163459047@qq.com>
2020-05-27 16:31:48 +0800

    add textview

--- app/src/main/java/com/yunzhifu/myapplication/MainActivity.java
+++ app/src/main/java/com/yunzhifu/myapplication/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = findViewById(R.id.tv);
+        mTextView.setText("89080");
     }
 }

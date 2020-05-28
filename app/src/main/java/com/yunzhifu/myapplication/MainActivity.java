de8de3c72ef3324f2fc12308ee811afc90130992
yukunkun <1163459047@qq.com>
2020-05-28 10:18:45 +0800

    add textview

--- app/src/main/java/com/yunzhifu/myapplication/MainActivity.java
+++ app/src/main/java/com/yunzhifu/myapplication/MainActivity.java
@@ -16,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
         mTextView = findViewById(R.id.tv);
 
-        mTextView.setText("近似的加送");
+        mTextView.setText("2334");
     }
 }
